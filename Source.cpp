040e2c8062630ab4695cd98dde062f417dfbb050
Yan <yan.simrok@gmail.com>
2022-03-08 18:53:59 +0300

    fix

--- Source.cpp
+++ Source.cpp
@@ -246,7 +246,7 @@ public:
 		{
 			push_front(value);
 		}
-		else if (index == size - 1)
+		else if (index == size)
 		{
 			push_back(value);
 		}
@@ -613,5 +613,32 @@ int main()
 {
 	srand(time(0));
 	system("chcp 1251");
-	
+	doublelist<int> one;
+	one.push_back(9);
+	one.push_front(3);
+	one.push_back(1);
+	one.push_back(4);
+	for (int i = 0; i < one.GetSize(); i++)
+	{
+		cout << one[i] << " ";
+	}
+	cout << endl;
+	one.insert(7, 3);
+	for (int i = 0; i < one.GetSize(); i++)
+	{
+		cout << one[i] << " ";
+	}
+	cout << endl;
+	one.removeAt(2);
+	for (int i = 0; i < one.GetSize(); i++)
+	{
+		cout << one[i] << " ";
+	}
+	cout << endl;
+	one.pop_front();
+	for (int i = 0; i < one.GetSize(); i++)
+	{
+		cout << one[i] << " ";
+	}
+	cout << endl;
 }
